b1c03e73f95066843675b9899ccd498e6dcdd7b9
arcanegolem <dopeflipper@gmail.com>
2025-02-22 17:28:57 +0300

    Updated example

--- app/src/main/java/arcanegolem/materialize/MainActivity.kt
+++ app/src/main/java/arcanegolem/materialize/MainActivity.kt
@@ -1,17 +1,24 @@
 package arcanegolem.materialize
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import arcanegolem.materialize.providers.PdfProvider
 import arcanegolem.materialize.ui.theme.MaterializeTheme
 import arcanegolem.materialize.viewer.ColumnPdfViewer
+import arcanegolem.materialize.viewer.PdfViewerColors
+import arcanegolem.materialize.viewer.PdfViewerOption
+import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +36,28 @@ class MainActivity : ComponentActivity() {
                 resId = R.raw.sample_multipage,
                 bitmapScale = 3,
                 identifier = "sample_multipage"
-              )
+              ),
+              colors = PdfViewerColors(
+                fillerPageColor = Color.Black,
+                floatingElementsColor = Color.Blue,
+                iconButtonColors = IconButtonDefaults.iconButtonColors(),
+                textFieldColors = TextFieldDefaults.colors()
+              ),
+              options = listOf(
+                PdfViewerOption(
+                  title = "SampleOption",
+                  action = {
+                    Toast.makeText(this@MainActivity, "Sample text", Toast.LENGTH_SHORT).show()
+                  }
+                ),
+                PdfViewerOption(
+                  title = "SampleOption2",
+                  action = {
+                    Toast.makeText(this@MainActivity, "Another sample text", Toast.LENGTH_SHORT).show()
+                  }
+                )
+              ),
+              fillerAspectRatio = 1f / sqrt(2f)
             )
           }
         }
