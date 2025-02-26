1ebcf76af4c2f4ef458a5a2a2b69f928772563fa
arcanegolem <dopeflipper@gmail.com>
2025-02-26 17:10:07 +0300

    Moved publishing to MavenCentral from JitPack

--- materialize/build.gradle.kts
+++ materialize/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jreleaser.model.Active
+
 plugins {
   alias(libs.plugins.android.library)
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.jreleaser)
   alias(libs.plugins.maven.publish)
 }
 
@@ -32,6 +35,7 @@ android {
   publishing {
     singleVariant("release") {
       withSourcesJar()
+      withJavadocJar()
     }
   }
 }
@@ -60,15 +64,93 @@ dependencies {
   implementation(libs.okhttp)
 }
 
+version = "1.0.0"
+
 publishing {
   publications {
-    register<MavenPublication>("release") {
-      groupId = "arcanegolem"
-      artifactId = "Materialize"
-      version = "1.0.0"
+    create<MavenPublication>("release") {
+      groupId = "io.github.arcanegolem"
+      artifactId = "materialize"
 
-      afterEvaluate {
-        from(components["release"])
+      pom {
+        name.set("materialize")
+        description.set("")
+        url.set("https://github.com/arcanegolem/Materialize")
+        issueManagement {
+          url.set("https://github.com/arcanegolem/Materialize/issues")
+        }
+
+        scm {
+          url.set("https://github.com/arcanegolem/Materialize")
+          connection.set("scm:git://github.com/arcanegolem/Materialize.git")
+          developerConnection.set("scm:git://github.com/arcanegolem/Materialize.git")
+        }
+
+        licenses {
+          license {
+            name.set("MIT License")
+            url.set("https://github.com/arcanegolem/Materialize/blob/master/LICENSE")
+            distribution.set("repo")
+          }
+        }
+
+        developers {
+          developer {
+            id.set("arcanegolem")
+            name.set("Mikhail Kleyzer")
+            email.set("dopeflipper@gmail.com")
+            url.set("")
+          }
+        }
+
+        afterEvaluate {
+          from(components["release"])
+        }
+      }
+    }
+  }
+  repositories {
+    maven {
+      setUrl(layout.buildDirectory.dir("staging-deploy"))
+    }
+  }
+}
+
+jreleaser {
+  jreleaser {
+    project {
+      inceptionYear = "2025"
+      author("@arcanegolem")
+    }
+    gitRootSearch = true
+    signing {
+      active = Active.ALWAYS
+      armored = true
+      verify = true
+    }
+    release {
+      github {
+        skipTag = true
+        sign = true
+        branch = "master"
+        branchPush = "master"
+        overwrite = true
+      }
+    }
+    deploy {
+      maven {
+        mavenCentral.create("sonatype") {
+          active = Active.ALWAYS
+          url = "https://central.sonatype.com/api/v1/publisher"
+          stagingRepository(layout.buildDirectory.dir("staging-deploy").get().toString())
+          setAuthorization("Basic")
+          applyMavenCentralRules = false
+          sign = true
+          checksums = true
+          sourceJar = true
+          javadocJar = true
+          retryDelay = 60
+        }
       }
     }
   }
